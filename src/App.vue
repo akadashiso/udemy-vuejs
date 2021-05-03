9b41851feea760e08fd9b892361e4f81e6a382d7
akadashiso <akadashiso@gmail.com>
2021-05-03 18:12:39 +0900

    [Finish]Session9 : こうすればVue.jsでフォームが簡単に作れる

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,48 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+    <div style="padding: 10rem;">
+      <h2>イベントのフォーム</h2>
+      <EventTitle v-model="eventData.title"></EventTitle>
+      <label for="maxNumber">最大人数</label>
+      <input
+        id="maxNumber"
+        type="number"
+        v-model.number="eventData.maxNumber"
+      >
+      <p>{{ eventData.maxNumber}}</p>
+      <label for="host">主催者</label>
+      <input
+        id="host"
+        type="text"
+        v-model.trim="eventData.host"
+      >
+      <pre>{{ eventData.host}}</pre>
+      <label for="detail">イベントの内容</label>
+      <textarea name="detail" id="" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <p style="white-space: pre;">{{ eventData.detail}}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate}}</p>
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="10">10代</label>
+      <input type="checkbox" id="10" value="20代" v-model="eventData.target">
+      <label for="20">20代</label>
+      <input type="checkbox" id="10" value="30代" v-model="eventData.target">
+      <label for="30">30代</label>
+      <p>{{ eventData.target}}</p>
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="free">有料</label>
+      <p>開催場所</p>
+      <select v-model="eventData.location" multiple>
+        <option v-for="location in locations" :key="location">{{ location }}</option>
+      </select>
+      <p>{{ eventData.location }}</p>
+    </div>
   </div>
 </template>
 
@@ -16,18 +58,31 @@
   import LikeHeader from "./components/LikeHeader.vue";
   import About from "./components/About.vue";
   import Home from "./components/Home.vue";
+  import EventTitle from "./components/EventTitle.vue";
 
   export default {
   data() {
     return {
       number: 14,
-      currentComponent: "Home"
+      currentComponent: "Home",
+      locations: ["東京", "大阪", "名古屋"],
+      eventData: {
+        title: "",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "無料",
+        location: []
+      },
     };
   },
     components: {
       LikeHeader,
       About,
-      Home
+      Home,
+      EventTitle
     },
     methods: {
       incrementNumber(value) {
@@ -36,13 +91,3 @@
     }
   };
 </script>
-
-<style scoped>
-div {
-  border: 1px solid red;
-}
-
-h1 {
-  color: blue;
-}
-</style>
--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -17,10 +17,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-h1 {
-  color: red;
-}
-</style>
