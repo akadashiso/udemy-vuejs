223090cf0b2ec4abfb7f20b96712741a34438d66
akadashiso <akadashiso@gmail.com>
2021-05-08 13:06:58 +0900

    [Finish]Section13-187 : active-class属性とexact属性を使って、アクティブなリンクにスタイリングを付ける

--- src/App.vue
+++ src/App.vue
@@ -1,189 +1,31 @@
 <template>
-  <div class="main">
-    <button @click="myAnimation = 'slide'">Slide</button>
-    <button @click="myAnimation = 'fade'">Fade</button>
-    <p>{{ myAnimation }}</p>
-    <br>
-    <button @click="add">追加</button>
-    <ul style="width: 200px; margin: auto;">
-      <transition-group name="fade">
-        <li
-          style="cursor: pointer;"
-          v-for="(number, index) in numbers"
-          :key="number"
-          @click="remove(index)"
-        >{{ number }}</li>
-      </transition-group>
-    </ul>
-    <br>
-    <button @click="show = !show">切り替え</button>
-    <br><br>
-    <transition
-      :css="false"
-      @before-enter="beforeEnter"
-      @enter="enter"
-      @leave="leave"
-    >
-      <div class="circle" v-if="show"></div>
-    </transition>
-    <button @click="myComponent = 'ComponentA'">ComponentA</button>
-    <button @click="myComponent = 'ComponentB'">ComponentB</button>
-    <transition name="fade" mode="out-in">
-      <component :is="myComponent"></component>
-    </transition>
-    <transition
-      name="fade"
-      mode="out-in"
-    >
-      <p
-        v-if="show"
-        key="bye"
-      >さよなら</p>
-      <p
-        v-else
-        key="hello"
-      >こんにちは</p>
-    </transition>
-    <transition
-      enter-active-class="animate__animated animate__bounce"
-      leave-active-class="animate__animated animate__shakeY"
-      appear
-    >
-      <p v-if="show">hello</p>
-    </transition>
-    <transition
-      :name="myAnimation"
-      appear
-    >
-      <p v-if="show">bye</p>
-    </transition>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link
+        to="/"
+        active-class="link--active"
+        exact
+        class="link"
+      >Home</router-link>
+      <router-link
+        to="/users"
+        active-class="link--active"
+        exact
+        class="link"
+      >Users</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ComponentA from "./components/ComponentA.vue";
-import ComponentB from "./components/ComponentB.vue";
-  export default {
-    components: {
-      ComponentA,
-      ComponentB
-    },
-    data() {
-      return {
-        numbers: [0, 1, 2],
-        nextNumber: 3,
-        show: true,
-        myAnimation: "slide",
-        myComponent: "componentA"
-      };
-    },
-    methods: {
-      randomIndex() {
-        return Math.floor(Math.random() * this.numbers.length);
-      },
-      add() {
-        this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
-        this.nextNumber += 1;
-      },
-      beforeEnter(el) {
-        el.style.transform = 'scale(0)'
-      },
-      remove(index) {
-        this.numbers.splice(index, 1);
-      },
-      enter(el, done) {
-        let scale = 0;
-        const interval = setInterval(() => {
-          el.style.transform = `scale(${scale})`;
-          scale += 0.1;
-          if (scale > 1) {
-            clearInterval(interval);
-            done();
-          }
-        }, 20);
-      },
-      leave(el, done) {
-        let scale = 1;
-        const interval = setInterval(() => {
-          el.style.transform = `scale(${scale})`;
-          scale -= 0.1;
-          if (scale < 0) {
-            clearInterval(interval);
-            done();
-          }
-        }, 20);
-      },
-    }
-  };
 </script>
 
 <style scoped>
-.circle {
-  width: 200px;
-  height: 200px;
-  margin: auto;
-  border-radius: 100px;
-  background-color: deeppink;
+.link {
+  margin-right: 10px;
 }
-
-.slide-enter,
-.slide-leave-to {
-  opacity: 0;
+.link--active {
+  font-size: 20px;
 }
-.slide-enter-active {
-  animation: slide-in 0.5s;
-  transition: opacity 0.5s;
-}
-
-.slide-enter-active {
-  animation: slide-in 0.5s;
-  transition: opacity 1s;
-
-}
-.slide-leave-active {
-  animation: slide-in 0.5s reverse;
-  transition: opacity 1s;
-}
-
-@keyframes slide-in {
-  from {
-    transform: translateX(100px)
-  }
-  to {
-    transform: translateX(0)
-  }
-}
-
-.fade-move {
-  transition: transform 1s;
-}
-
-.fade-enter {
-  opacity: 0;
-}
-.fade-enter-active {
-  transition: opacity 1s;
-}
-.fade-enter-to {
-  opacity: 1;
-}
-.fade-leave {
-  opacity: 1;
-}
-.fade-leave-active {
-  transition: opacity 1s;
-  position: absolute;
-  width: 200px;
-}
-.fade-leave-to {
-  opacity: 0;
-}
-
-.main {
-  width: 70%;
-  margin: auto;
-  padding-top: 5rem;
-  text-align: center;
-}
-
 </style>
